3f47eab708e5861a7e1270e6f325459fd38ce1fe
Mikhail Mudrogelov <mudrogelov.beled@gmail.com>
2020-10-03 00:03:37 +0300

    feat: solve dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  let firstLetters = [];
+  for (let member of members) {
+    if (typeof(member) === 'string') {
+        firstLetters.push(member.trim().charAt(0).toUpperCase())
+    }
+  }
+  return firstLetters.sort().join('');
 };
