d1014d6dae29c4aa906f693741b10319e6b258f2
Mikhail Mudrogelov <mudrogelov.beled@gmail.com>
2020-10-04 19:37:37 +0300

    feat: solve transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,50 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+  if (Array.isArray(arr) === false) throw new Error;
+  if (arr.length === 0) return arr;
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    /*
+    if (typeof(arr[i]) === 'boolean') {
+      i++;
+    }
+    */
+   /*
+   if (typeof(arr[i]) === 'object') {
+     newArray.push(arr[i]);
+   }
+   */
+    switch (arr[i]) {
+      case '--discard-next':
+        if (arr[i + 2] === '--discard-prev' || arr[i + 2] === '--double-prev') {
+          i += 2;
+        } else i++
+        break;
+
+      case '--discard-prev':
+        if (i > 0) {
+          newArray.pop();
+        }
+        break;
+        
+      case '--double-next':
+        if (i < arr.length - 1) {
+          newArray.push(arr[i + 1]);
+        }
+        break;
+        
+      case '--double-prev':
+        if (i > 0) {
+          newArray.push(arr[i - 1])
+        };
+        break;
+
+      default:
+        newArray.push(arr[i]);
+        break;
+    }
+  }
+  return newArray;
 };
